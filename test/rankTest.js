e8d92132d669b0b0401703d8ff067efd17e5cb28
Austin <497133212@qq.com>
2020-08-28 21:02:33 +0800

    [refactoring-homework][Austin] test: add rank test case 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,126 +1,196 @@
-import {test} from "ava";
 import {rating} from "../src/rank";
+
 const rankTest = require('ava');
 
 rankTest('foo', t => {
-  t.pass();
+    t.pass();
 });
 
 rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+    const bar = Promise.resolve('bar');
+    t.is(await bar, 'bar');
 });
 
 rankTest('rank case 1. given voyage and history when rating then return A', t => {
-  const voyage = {
-    zone: 'china',
-    length: 10,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+    const voyage = {
+        zone: 'china',
+        length: 10,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+    ];
 
-  let result = rating(voyage, history);
+    let result = rating(voyage, history);
 
-  // then
-  t.is(result, 'A');
+    // then
+    t.is(result, 'A');
 });
 
 rankTest('rank case 2. given voyage and history when rating then return B', t => {
-  const voyage = {
-    zone: 'east-indies',
-    length: 10,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-  ];
+    const voyage = {
+        zone: 'east-indies',
+        length: 10,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+    ];
 
-  let result = rating(voyage, history);
+    let result = rating(voyage, history);
 
-  // then
-  t.is(result, 'B');
+    // then
+    t.is(result, 'B');
 });
 
 rankTest('rank case 3. given voyage and history when rating then return B', t => {
-  const voyage = {
-    zone: 'china',
-    length: 19,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    }
-  ];
+    const voyage = {
+        zone: 'china',
+        length: 19,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        }
+    ];
 
-  let result = rating(voyage, history);
+    let result = rating(voyage, history);
 
-  // then
-  t.is(result, 'B');
+    // then
+    t.is(result, 'B');
+});
+
+rankTest('rank case 4. given voyage and history when rating then return B', t => {
+    const voyage = {
+        zone: 'east-indies',
+        length: 19,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        }
+    ];
+
+    let result = rating(voyage, history);
+
+    // then
+    t.is(result, 'B');
 });
 
 
