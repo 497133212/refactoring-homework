1c367290071b97dbafc80b5473a52bb404da949b
Austin <497133212@qq.com>
2020-08-28 20:56:11 +0800

    [refactoring-homework][Austin] test: add rank test case 3

--- test/rankTest.js
+++ test/rankTest.js
@@ -39,5 +39,88 @@ rankTest('rank case 1. given voyage and history when rating then return A', t =>
   t.is(result, 'A');
 });
 
+rankTest('rank case 2. given voyage and history when rating then return B', t => {
+  const voyage = {
+    zone: 'east-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+
+  let result = rating(voyage, history);
+
+  // then
+  t.is(result, 'B');
+});
+
+rankTest('rank case 3. given voyage and history when rating then return B', t => {
+  const voyage = {
+    zone: 'china',
+    length: 19,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    }
+  ];
+
+  let result = rating(voyage, history);
+
+  // then
+  t.is(result, 'B');
+});
 
 
