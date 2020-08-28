1b874516ee0380c0621ab3e8085c35971e443b65
Austin <497133212@qq.com>
2020-08-28 21:13:57 +0800

    [refactoring-homework][Austin] refactor: refactor calculateVoyageProfitFactor method

--- src/rank.js
+++ src/rank.js
@@ -1,102 +1,104 @@
-function calculateVoyageRisk (voyage) {
-  let result = 1;
-  if (voyage.length > 4) {
-    result += 2;
-  }
-  if (voyage.length > 8) {
-    result += voyage.length - 8;
-  }
-  if ([
-    'china',
-    'east-indies',
-  ].includes(voyage.zone)) {
-    result += 4;
-  }
-  return Math.max(result, 0);
+function calculateVoyageRisk(voyage) {
+    let result = 1;
+    if (voyage.length > 4) {
+        result += 2;
+    }
+    if (voyage.length > 8) {
+        result += voyage.length - 8;
+    }
+    if ([
+        'china',
+        'east-indies',
+    ].includes(voyage.zone)) {
+        result += 4;
+    }
+    return Math.max(result, 0);
 }
 
-function hasChina (history) {
-  return history.some(v => 'china' === v.zone);
+function hasChina(history) {
+    return history.some(v => 'china' === v.zone);
 }
 
-function calculateCaptainHistoryRisk (voyage, history) {
-  let result = 1;
-  if (history.length < 5) {
-    result += 4;
-  }
-  result += history.filter(v => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result -= 2;
-  }
-  return Math.max(result, 0);
+function calculateCaptainHistoryRisk(voyage, history) {
+    let result = 1;
+    if (history.length < 5) {
+        result += 4;
+    }
+    result += history.filter(v => v.profit < 0).length;
+    if (voyage.zone === 'china' && hasChina(history)) {
+        result -= 2;
+    }
+    return Math.max(result, 0);
 }
 
-function calculateVoyageProfitFactor (voyage, history) {
-  let result = 2;
-  if (voyage.zone === 'china') {
-    result += 1;
-  }
-  if (voyage.zone === 'east-indies') {
-    result += 1;
-  }
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
-      result += 1;
+function calculateVoyageProfitFactor(voyage, history) {
+    let result = 2;
+    if (voyage.zone === 'china') {
+        result += 1;
+        if(hasChina(history)) {
+            result += 3;
+            if (history.length > 10) {
+                result += 1;
+            }
+            if (voyage.length > 12) {
+                result += 1;
+            }
+            if (voyage.length > 18) {
+                result -= 1;
+            }
+        }else {
+            if (history.length > 8) {
+                result += 1;
+            }
+            if (voyage.length > 14) {
+                result -= 1;
+            }
+        }
     }
-    if (voyage.length > 12) {
-      result += 1;
+    if (voyage.zone === 'east-indies') {
+        result += 1;
     }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
-  }
-  else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
-  }
-  return result;
+    return result;
 }
 
-function rating (voyage, history) {
-  const voyageProfitFactor = calculateVoyageProfitFactor(voyage, history);
-  const voyageRisk = calculateVoyageRisk(voyage);
-  const captainHistoryRisk = calculateCaptainHistoryRisk(voyage, history);
-  if (voyageProfitFactor * 3 > (voyageRisk + captainHistoryRisk * 2)) {
-    return 'A';
-  }
-  else {
-    return 'B';
-  }
+function printResult(voyage, history) {
+    const voyageProfitFactor = calculateVoyageProfitFactor(voyage, history);
+    const voyageRisk = calculateVoyageRisk(voyage);
+    const captainHistoryRisk = calculateCaptainHistoryRisk(voyage, history);
+    if (voyageProfitFactor * 3 > (voyageRisk + captainHistoryRisk * 2)) {
+        return 'A';
+    } else {
+        return 'B';
+    }
+}
+
+function rating(voyage, history) {
+    return printResult(voyage, history)
 }
 
 module.exports = {
-  rating
+    rating
 };
 
 const voyage = {
-  zone: 'west-indies',
-  length: 10,
+    zone: 'west-indies',
+    length: 10,
 };
 const history = [
-  {
-    zone: 'east-indies',
-    profit: 5,
-  },{
-    zone: 'west-indies',
-    profit: 15,
-  },{
-    zone: 'china',
-    profit: -2,
-  },
-  {
-    zone: 'west-africa',
-    profit: 7,
-  },
+    {
+        zone: 'east-indies',
+        profit: 5,
+    }, {
+        zone: 'west-indies',
+        profit: 15,
+    }, {
+        zone: 'china',
+        profit: -2,
+    },
+    {
+        zone: 'west-africa',
+        profit: 7,
+    },
 ];
 const myRating = rating(voyage, history);
 console.log(`myRating: ${myRating}`);
